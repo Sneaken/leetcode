046e5cf0f1eef0c66a73fca1ed028b68c0d6b6c0
Sneaken <924393527@qq.com>
2022-05-05 22:11:11 +0800

    41

--- array/41.js
+++ array/41.js
@@ -14,24 +14,35 @@
 //   1 <= nums.length <= 5 * 10^5
 //   -2^31 <= nums[i] <= 2^31 - 1
 
-const nums = [3, 4, -1, 1]
+const nums = [-1, 0, 2]
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var firstMissingPositive = function (nums) {
   const n = nums.length
+  // 情况1: nums 是按顺序排列 即 [1,2,3,....,n], 此时最小的正数应该是 n + 1
+  // 情况2: nums: [i-1,i+1,...,n], 此时最小的正数应该是i
   for (let i = 0; i < n; i++) {
     // 先排除负数
     nums[i] <= 0 && (nums[i] = n + 1)
   }
+  // [-1, 0, 2】
+  // [ 3, 3, 2]
+
   // [0, n - 1] => [1, n]
   for (let i = 0; i < n; i++) {
-    if (nums[i] <= n) {
-      nums[Math.abs(nums[i]) - 1] = -Math.abs(nums[Math.abs(nums[i]) - 1])
+    const current = nums[i]
+    if (current <= n) {
+      const idx = Math.abs(current) - 1
+      // 保证一定是负数
+      nums[idx] = -Math.abs(nums[idx])
     }
   }
+  // [3,  3,  2]
+  // [3, -3, -2]
   for (let i = 0; i < n; i++) {
+    // 如果 idx 位置的值 > 0, 表示 idx + 1 没有出现过
     if (nums[i] > 0) return i + 1
   }
   // 范围内都在, 那缺少的就是 n + 1
