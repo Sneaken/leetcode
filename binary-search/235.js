626957eb1a27665d4b14283df7214dcbc7744bf6
Sneaken <924393527@qq.com>
2022-07-21 21:15:40 +0800

    235

---
+++ binary-search/235.js
@@ -0,0 +1,30 @@
+// 235. 二叉搜索树的最近公共祖先
+// 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+//
+// 例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+
+// 重点是 BST
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const lowestCommonAncestor = function (root, p, q) {
+  if (root == null) return null
+  if (p.val > q.val) return lowestCommonAncestor(root, q, p)
+  if (root.val >= p.val && root.val <= q.val) return root
+  if (root.val > q.val) return lowestCommonAncestor(root.left, p, q)
+  return lowestCommonAncestor(root.right, p, q)
+}
