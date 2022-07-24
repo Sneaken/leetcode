c67206fede773ffafd33492e629c2af361c5274a
Sneaken <924393527@qq.com>
2022-07-24 09:57:01 +0800

    feat: 生成题目到本地

---
+++ scripts/clone-question.js
@@ -0,0 +1,112 @@
+import fs from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const headers = {
+  accept: "*/*",
+  "accept-language": "zh-CN",
+  "cache-control": "no-cache",
+  "content-type": "application/json",
+  pragma: "no-cache",
+  "sec-ch-ua": '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+  "sec-ch-ua-mobile": "?0",
+  "sec-ch-ua-platform": '"macOS"',
+  "sec-fetch-dest": "empty",
+  "sec-fetch-mode": "cors",
+  "sec-fetch-site": "same-origin",
+  "x-definition-name": "question",
+  "x-operation-name": "questionData",
+  "x-timezone": "Asia/Shanghai",
+  "Referrer-Policy": "strict-origin-when-cross-origin",
+};
+
+const isFileExists = async (path) => {
+  try {
+    await fs.stat(path);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+function formatter(content, codeSnippet) {
+  return (
+    "// " +
+    content
+      // 去掉换行符后面的空格
+      .replace(/\n\s+/g, "\n")
+      // 合并多个换行符为一个 并加上 ‘//’
+      .replace(/\n+/g, "\n// ")
+      // 去掉 md 的 引用语法
+      .replaceAll("// >", "// ")
+      // 去掉 '// ' 行
+      .replace(/\/\/\s{1,}\n/g, "")
+      // 去掉 md 的 加粗语法
+      .replace(/\*\*(.*?)\*\*/g, ($0, $1) => $1) +
+    "\n\n" +
+    codeSnippet
+  );
+}
+
+async function cloneQuestion(titleSlug, dirname) {
+  if (!titleSlug || !dirname) return;
+  const {
+    data: { question },
+  } = await fetch("https://leetcode.cn/graphql/", {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      operationName: "questionData",
+      variables: { titleSlug },
+      query: `query questionData($titleSlug: String!) {
+        question(titleSlug: $titleSlug) {
+          questionId
+          questionFrontendId
+          categoryTitle
+          title
+          titleSlug
+          translatedTitle
+          translatedContent
+          difficulty
+          codeSnippets {
+            lang
+            langSlug
+            code
+          }
+          topicTags {
+            name
+            slug
+            translatedName
+          }
+          sampleTestCase
+          metaData
+          editorType
+          style
+          exampleTestcases
+          jsonExampleTestcases
+        }
+      }`,
+    }),
+  }).then((res) => res.json());
+
+  const { questionFrontendId, translatedContent, codeSnippets } = question;
+
+  const filePath = join(__dirname, "..", dirname, questionFrontendId.replaceAll(" ", "") + ".js");
+  const isExists = await isFileExists(filePath);
+  const jsCodeSnippet = codeSnippets.find((codeSnippet) => codeSnippet.lang === "JavaScript").code;
+  if (isExists) {
+    // 原来的文件备份
+    const time = new Date();
+    fs.rename(filePath, filePath.replace(".js", `-${time.toLocaleString().replace(/[ /:]/g, "-")}.js`));
+  }
+  fs.writeFile(filePath, formatter(translatedContent, jsCodeSnippet));
+}
+
+// https://leetcode.cn/problems/:titleSlug/
+const titleSlug = "";
+// 项目目录下的文件夹路径（把题目放于以下路径中）
+const _dirname = "";
+
+cloneQuestion(titleSlug, _dirname);
