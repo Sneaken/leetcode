7e8816bbf58ead2c5d990253ad0b7e40ca3b61fd
Sneaken <924393527@qq.com>
2022-07-28 08:31:44 +0800

    101
    执行用时：80 ms, 在所有 JavaScript 提交中击败了11.70%的用户
    内存消耗：43.3 MB, 在所有 JavaScript 提交中击败了89.34%的用户

--- tree-node/101.js
+++ tree-node/101.js
@@ -1,80 +1,59 @@
+// https://leetcode.cn/problems/symmetric-tree
+// 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+// 示例 1：
+// 输入：root = [1,2,2,3,4,4,3]
+// 输出：true
+// 示例 2：
+// 输入：root = [1,2,2,null,3,null,3]
+// 输出：false
+// 提示：
+// 树中节点数目在范围 [1, 1000] 内
+// -100 = Node.val = 100
+// 进阶：你可以运用递归和迭代两种方法解决这个问题吗？
+
 import { changeArrToTreeNode } from "./index.js";
 
-const root = [1, 2, 2, 3, 4, 4, 3, 1];
+const root = [1, 2, 2, 3, 4, 4, 3];
 const rootTree = changeArrToTreeNode(root);
-
-console.log("rootTree =>", rootTree);
-const test = () => {
-  const path = [];
-  let maxDepth = 0;
-  let depth = 0;
-  const dfs = (root) => {
-    if (!root) {
-      console.log("depth =>", depth);
-      maxDepth = Math.max(maxDepth, depth);
-      return;
-    }
-    depth++;
-    dfs(root.left);
-    dfs(root.right);
-    depth--;
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  const check = (p, q) => {
+    if (!p && !q) return true;
+    if (!p || !q || p.val !== q.val) return false;
+    return check(p.left, q.right) && check(p.right, q.left);
   };
-  dfs(rootTree);
-  console.log("path =>", path);
+  return check(root, root);
 };
 
-test();
+var isSymmetric2 = function (root) {
+  const check = (p, q) => {
+    const queue = [p, q];
+    while (queue.length) {
+      p = queue.shift();
+      q = queue.shift();
+      if (!p && !q) continue;
+      if (!p || !q || p.val !== q.val) return false;
+      queue.push(p.left);
+      queue.push(q.right);
 
-const isSymmetric = function (root) {
-  if (!root) return false;
-  const queue = [root];
-  while (queue.length) {
-    const len = queue.length;
-    const level = [];
-    for (let i = 0; i < len; i++) {
-      const node = queue.shift();
-      if (node) {
-        level.push(node.val);
-        queue.push(node.left);
-        queue.push(node.right);
-      } else {
-        level.push("None");
-      }
-    }
-    if ([...level].reverse().toString() !== level.toString()) return false;
-  }
-
-  return true;
-};
-
-const isSymmetric2 = function (root) {
-  const check = (u, v) => {
-    const q = [];
-    q.push(u), q.push(v);
-
-    while (q.length) {
-      u = q.shift();
-      v = q.shift();
-
-      if (!u && !v) continue;
-      if (!u || !v || u.val !== v.val) return false;
-
-      q.push(u.left);
-      q.push(v.right);
-
-      q.push(u.right);
-      q.push(v.left);
+      queue.push(p.right);
+      queue.push(q.left);
     }
     return true;
   };
   return check(root, root);
 };
 
-const isSymmetric3 = function (root) {
-  const check = (p, q) => {
-    if (!p && !q) return true;
-    if (!p || !q) return false;
-    return p.val === q.val && check(p.left, q.right) && check(p.right, q.left);
-  };
-  return check(root, root);
-};
+console.log("isSymmetric(rootTree) => ", isSymmetric(rootTree));
+console.log("isSymmetric2(rootTree) => ", isSymmetric2(rootTree));
