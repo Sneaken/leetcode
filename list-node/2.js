7f2d597ac9b7318def717212c7f4be67f9841cac
Sneaken <924393527@qq.com>
2022-12-22 10:28:09 +0800

    2

---
+++ list-node/2.js
@@ -0,0 +1,88 @@
+// https://leetcode.cn/problems/add-two-numbers/
+// 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+// 请你将两个数相加，并以相同形式返回一个表示和的链表。
+// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+// 示例 1：
+// <img alt="" src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2021/01/02/addtwonumber1.jpg" style="width: 483px; height: 342px;" />
+// 输入：l1 = [2,4,3], l2 = [5,6,4]
+// 输出：[7,0,8]
+// 解释：342 + 465 = 807.
+// 示例 2：
+// 输入：l1 = [0], l2 = [0]
+// 输出：[0]
+// 示例 3：
+// 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// 输出：[8,9,9,9,0,0,0,1]
+// 提示：
+// 每个链表中的节点数在范围 [1, 100] 内
+// 0 <= Node.val <= 9
+// 题目数据保证列表表示的数字不含前导零
+import { changeArrToListNode, ListNode } from './index.js';
+
+const l1 = changeArrToListNode([2, 4, 3]);
+const l2 = changeArrToListNode([5, 6, 4]);
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  let ans = new ListNode();
+  let dummyNode = ans;
+
+  while (l1 || l2) {
+    const val = (l1?.val || 0) + (l2?.val || 0) + (ans?.val || 0);
+    ans.val = val % 10;
+    if (val >= 10) {
+      ans.next = new ListNode((val / 10) | 0);
+    } else if (l1?.next || l2?.next) {
+      ans.next = new ListNode();
+    }
+    ans = ans.next;
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
+    }
+  }
+  return dummyNode;
+};
+
+// 官方题解
+var addTwoNumbers2 = function (l1, l2) {
+  let head = null,
+    tail = null;
+  let carry = 0;
+  while (l1 || l2) {
+    const n1 = l1 ? l1.val : 0;
+    const n2 = l2 ? l2.val : 0;
+    const sum = n1 + n2 + carry;
+    if (!head) {
+      head = tail = new ListNode(sum % 10);
+    } else {
+      tail.next = new ListNode(sum % 10);
+      tail = tail.next;
+    }
+    carry = Math.floor(sum / 10);
+    if (l1) {
+      l1 = l1.next;
+    }
+    if (l2) {
+      l2 = l2.next;
+    }
+  }
+  if (carry > 0) {
+    tail.next = new ListNode(carry);
+  }
+  return head;
+};
+
+console.log('addTwoNumbers(l1, l2) =>', addTwoNumbers(l1, l2));
